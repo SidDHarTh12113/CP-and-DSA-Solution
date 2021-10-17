61deecb335b60e67a214c508317536a4f57a0589
Akansha2202 <50712974+Akansha2202@users.noreply.github.com>
2021-10-17 23:27:49 +0530

    Using BFS for Cycle Detection

--- B.cpp
+++ B.cpp
@@ -1,40 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
 class Solution {
 public:
-  bool isBipartiteHelper(vector<vector<int>>graph, vectro<int>& vis, int ele) {
-    queue<int>q;
-    q.push(ele);
-    vis[ele] = 1;
+  bool isCycleHelper(int V, vector<int> adj[], int ele, vector<bool>& vis) {
+    // Code here
+    queue<pair<int, int>>q;
+    q.push({ele, -1});
+    vis[ele] = true;
     while (!q.empty()) {
-      int x = q.front();
+      int fir = q.front().first;
+      int sec = q.front().second;
       q.pop();
-      for (auto p : graph[x]) {
-        if (vis[p] != -1) {
-          if (vis[p] == vis[x]) {
-            return false;
-          }
+      for (auto p : adj[fir]) {
+        if (vis[p] == false) {
+          q.push({p, fir});
+          vis[p] = true;
         }
-        else {
-          if (vis[x] == 1) {
-            vis[p] = 0;
-          }
-          else {
-            vis[p] = 1;
-          }
-          q.push(p);
+        else if (sec != p) {
+          return true;
         }
       }
     }
-    return true;
+    return false;
   }
-  bool isBipartite(vector<vector<int>>& graph) {
-    vector<int>vis(graph.size(), -1);
-    for (int i = 0; i < graph.size(); i++) {
-      if (vis[i] == -1) {
-        if (isBipartiteHelper(graph, vis, i) == false) {
-          return false;
+  bool isCycle(int V, vector<int> adj[]) {
+    vector<bool>vis(V + 1, false);
+    for (int i = 0; i < V; i++) {
+      if (vis[i] == false) {
+        if (isCycleHelper(V, adj, i, vis)) {
+          return true;
         }
       }
     }
-    return true;
+    return false;
   }
 };
+
+// { Driver Code Starts.
+int main() {
+  int tc;
+  cin >> tc;
+  while (tc--) {
+    int V, E;
+    cin >> V >> E;
+    vector<int> adj[V];
+    for (int i = 0; i < E; i++) {
+      int u, v;
+      cin >> u >> v;
+      adj[u].push_back(v);
+      adj[v].push_back(u);
+    }
+    Solution obj;
+    bool ans = obj.isCycle(V, adj);
+    if (ans)
+      cout << "1\n";
+    else
+      cout << "0\n";
+  }
+  return 0;
+}  // } Driver Code Ends
